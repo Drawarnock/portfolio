b45acd6a5f38e0594abfe202680277f93b226a08
Konrad Wisłocki <wislockik23@gmail.com>
2017-11-21 21:49:42 +0100

    added menu.css, script menu.js for menu, menu is not working for 100%

---
+++ js/menu.js
@@ -0,0 +1,77 @@
+(function (cb) {
+
+    // browser event has already occurred.
+    if (document.readyState === "complete") {
+        return setTimeout(cb);
+    }
+
+    // Mozilla, Opera and webkit style
+    if (window.addEventListener) {
+        return window.addEventListener("load", cb, false);
+    }
+
+    // If IE event model is used
+    if (window.attachEvent) {
+        return window.attachEvent("onload", cb);
+    }
+
+})(function () {
+
+    document.querySelectorAll('.nav__btn').forEach(function (el) {
+        el.addEventListener('click', function () {
+            var nav = this.parentElement.parentElement,
+                _class = 'nav--open';
+
+            nav.classList.contains(_class) ?
+                nav.classList.remove(_class) :
+                nav.classList.add(_class)
+        });
+    });
+
+});
+
+
+
+(function () {
+    let windowWidth = window.innerWidth;
+    let hiddenButton = document.querySelector('.nav__btn--hidden');
+    let visibleButtons = document.querySelectorAll('.main');
+
+    if (windowWidth < 768) {
+
+        hiddenButton.classList.remove('nav__btn--hidden');
+        visibleButtons.forEach(function (el) {
+                el.classList.add('nav__btn--hidden');
+            });
+        }
+        else {
+            hiddenButton.classList.add('nav__btn--hidden');
+        
+        visibleButtons.forEach(function (el) {
+                el.classList.remove('nav__btn--hidden');
+        });
+    }
+
+    window.addEventListener('resize', function () {
+
+        let windowWidth = window.innerWidth;
+        if (windowWidth < 768) {
+            hiddenButton.classList.remove('nav__btn--hidden');
+        } else {
+            hiddenButton.classList.add('nav__btn--hidden');
+        }
+    });
+
+    window.addEventListener('resize', function () {
+        let windowWidth = window.innerWidth;
+        visibleButtons.forEach(function (el) {
+            if (windowWidth < 768) {
+                el.classList.add('nav__btn--hidden');
+
+            } else {
+                el.classList.remove('nav__btn--hidden');
+            }
+        });
+    });
+
+})();
